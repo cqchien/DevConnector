fa4d27b7de8fafb0a5465adf0ef6529e000da925
cqchien <caochientp1@gmail.com>
2021-03-09 00:01:40 +0700

    Set up MongoDB

---
+++ config/connect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const dbUrl = config.get("mongoURL");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected.....");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const connectDB = require("./config/connect");
 
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => res.send("API sending"));
 
 const PORT = process.env.PORT || 5000;
